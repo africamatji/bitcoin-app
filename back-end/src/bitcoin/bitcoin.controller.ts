8cedb379ef0e3d163b328d458a466867aad72526
africam <africamatji@gmail.com>
2023-04-21 19:09:31 +0200

    default exchange rate to USD

--- back-end/src/bitcoin/bitcoin.controller.ts
+++ back-end/src/bitcoin/bitcoin.controller.ts
@@ -5,6 +5,11 @@ import { BitCoinService } from './bitcoin.service';
 export class BitCoinController {
   constructor(private readonly bitcoinService: BitCoinService) {}
 
+  @Get('exchange')
+  getDefaultExchangeRate(): object {
+    return this.bitcoinService.getExchangeRateInCurrency('usd')
+  }
+
   @Get('exchange/:currency')
   getExchangeRateInCurrency(@Param('currency') currency: string): object {
     return this.bitcoinService.getExchangeRateInCurrency(currency)
